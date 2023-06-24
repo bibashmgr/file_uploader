4b96694621165fa736be1143acd73dd6bfd37e04
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-06-24 23:22:36 +0545

    [update]: env

--- server/utils/config.js
+++ server/utils/config.js
@@ -5,7 +5,7 @@ const nodeEnv = process.env.NODE_ENV;
 const portNumber = process.env.PORT_NUMBER || 9999;
 const clientBaseUrl = process.env.CLIENT_BASE_URL;
 const serverBaseUrl = process.env.SERVER_BASE_URL;
-const mongodbUrl = process.env.MONGODB_LOCAL_URL;
+const mongodbUrl = process.env.MONGODB_URL;
 
 module.exports = {
   nodeEnv,
