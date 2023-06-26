22fcc67e5591f0cd0cdea03ebd3e80bf836ed1c2
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-06-26 14:00:59 +0545

    [update]: upload.helper

---
+++ server/helpers/upload.helper.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const crypto = require('crypto');
+const { GridFsStorage } = require('multer-gridfs-storage');
+const multer = require('multer');
+
+// utils
+const config = require('../utils/config.js');
+
+const storage = new GridFsStorage({
+  url: config.mongodbUrl,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads',
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = { upload };
